1646a3b94a20382b67053677fe7c3a9ff2382f24
giuliaserenaorlando <giuliaserenaorlando@GIULIASEREN0108>
2023-08-30 16:30:32 +0200

    Aggiungere i file di progetto.

---
+++ U3W1D3_ContoCorrente/ContoCorrente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3W1D3_ContoCorrente
+{
+    internal class ContoCorrente
+    {
+        public string Titolare { get; set; }
+        public string IBAN { get; set; }
+        public DateTime DataApertura { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public bool Aperto { get; set; }
+
+        //Metodi
+
+        private string GeneraNumeroConto()
+        {
+            Random random = new Random();
+            int random1 = random.Next(10000, 100000);
+            int random2 = random.Next(10000, 100000);
+            string numeroConto = "IT" + random1.ToString() + "0000" + random2.ToString();
+            return numeroConto;
+        }
+        public void AperturaConto(string nome, string cognome, decimal importoIniziale)
+        {
+            if (!Aperto && importoIniziale >= 1000)
+            {
+                Titolare = nome + " " + cognome;
+                IBAN = GeneraNumeroConto();
+                Saldo = importoIniziale;
+                DataApertura = DateTime.Now;
+                Aperto = true;
+
+                Console.WriteLine("Conto aperto con successo. IBAN: " + IBAN + " - Saldo iniziale: " + Saldo);
+            }
+            else
+            {
+                Console.WriteLine("Impossibile aprire il conto.");
+            }
+
+
+        }
+
+        public void Versamento(decimal importo)
+        {
+            if (Aperto && importo > 0)
+            {
+                Saldo += importo;
+                Console.WriteLine($"Versamento di {importo} € effettuato. Saldo attuale: {Saldo} €");
+            }
+            else
+            {
+                Console.WriteLine("Impossibile effettuare il versamento.");
+            }
+        }
+        public void Prelievo(decimal importo)
+        {
+            if (Aperto && Saldo > 0)
+            {
+                Saldo -= importo;
+                Console.WriteLine($"Prelievo di {importo} € effettuato. Saldo attuale: {Saldo} €");
+            }
+        
+            else
+            {
+                Console.WriteLine("Impossibile effettuare il prelievo");
+            }
+        }
+    }
+}
---
+++ U3W1D3_ContoCorrente/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3W1D3_ContoCorrente
+{
+    internal class Program
+    {
+
+        private void MostraMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Apri conto");
+            Console.WriteLine("2. Effettua versamento");
+            Console.WriteLine("3. Effettua prelievo");
+            Console.WriteLine("4. Visualizza elenco conti");
+            Console.WriteLine("5. Esci");
+            Console.Write("Scelta: ");
+        }
+        static void Main(string[] args)
+        {
+
+            {
+                Program program = new Program();
+                List<ContoCorrente> conti = new List<ContoCorrente>();
+                ContoCorrente conto = null;
+
+                while (true)
+                {
+                    program.MostraMenu();
+                    int scelta = int.Parse(Console.ReadLine());
+
+                    switch (scelta)
+                    {
+                        case 1:
+                            Console.Write("Nome: ");
+                            string Nome = Console.ReadLine();
+                            Console.Write("Cognome: ");
+                            string Cognome = Console.ReadLine();
+                            Console.Write("Importo iniziale: ");
+                            decimal importoIniziale = Convert.ToDecimal(Console.ReadLine());
+
+                            // Controlla se il conto è già presente
+                            if (conti.Exists(c => c.Titolare == Nome + " " + Cognome))
+                            {
+                                Console.WriteLine("Titolare già presente.");
+                            }
+                            else
+                            {
+                                conto = new ContoCorrente();
+                                conto.AperturaConto(Nome, Cognome, importoIniziale);
+                                conti.Add(conto);
+                            }
+                            break;
+
+                        case 2:
+                            if (conto != null)
+                            {
+                                Console.Write("Importo versamento: ");
+                                decimal importoVersamento = Convert.ToDecimal(Console.ReadLine());
+                                conto.Versamento(importoVersamento);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Apri un conto prima di effettuare un versamento.");
+                            }
+                            break;
+
+                        case 3:
+                            if (conto != null)
+                            {
+                                Console.Write("Importo prelievo: ");
+                                decimal importoPrelievo = Convert.ToDecimal(Console.ReadLine());
+                                conto.Prelievo(importoPrelievo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Apri un conto prima di effettuare un prelievo.");
+                            }
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Elenco conti:");
+                            foreach (ContoCorrente contoCorrente in conti)
+                            {
+                                Console.WriteLine(contoCorrente.IBAN);
+                            }
+                            break;
+
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Scelta non valida.");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
